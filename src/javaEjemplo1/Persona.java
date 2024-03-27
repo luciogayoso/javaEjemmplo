b74583879ceeee037fe9bd613a1c55baf1846de4
luciogayoso <luciogayoso@gmail.com>
2024-03-26 21:54:37 -0300

    Clase Persona completo;

--- src/javaEjemplo1/Persona.java
+++ src/javaEjemplo1/Persona.java
@@ -1,5 +1,35 @@
 package javaEjemplo1;
 
 public class Persona {
+private String nombre;
+private String apellido;
+private int edad;
 
+public String saludar() {
+	return "Hola soy "+ nombre + " y mi apellido es "+ apellido+ " tengo "+ edad + "años";
+}
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+public String getApellido() {
+	return apellido;
+}
+
+public void setApellido(String apellido) {
+	this.apellido = apellido;
+}
+
+public int getEdad() {
+	return edad;
+}
+
+public void setEdad(int edad) {
+	this.edad = edad;
+}
 }
